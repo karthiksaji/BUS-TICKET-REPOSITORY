de9a5a7bedd84606f2972cbd0d3858266beb4366
sajeevkarthik87@gmail.com <kl05au@8938>
2025-01-20 17:23:46 +0530

    added modules such as user,admin and also installed packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'bus_ticket_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+    AdminModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
